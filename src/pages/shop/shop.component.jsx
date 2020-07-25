e9426fa37fddc04df65879020df772c310c5a2bd
duykhanhtran1808 <duykhanhtran1808@gmail.com>
2020-07-25 12:07:39 +0700

    add header logo and links

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import PreviewCollection from  "../../components/preview-collection/preview-collection.component"
+import PreviewCollection from "../../components/preview-collection/preview-collection.component";
 
-import SHOP_DATA from './shop.data.js'
+import SHOP_DATA from "./shop.data.js";
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
 
-    }
-
-    render () {
-        const {collections} = this.state;
-        return (<div className="shop-page">
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <PreviewCollection key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>)
-    }
-
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <PreviewCollection key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
