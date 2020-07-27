3349860122c5d40ad46104e6ac4197d2d6624afa
duykhanhtran1808 <duykhanhtran1808@gmail.com>
2020-07-27 16:52:23 +0700

    redirect

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,17 +1,19 @@
+import { UserActionType } from "./user.types";
+
 const INITIAL_STATE = {
-    currentUser: null
-}
+  currentUser: null,
+};
 
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'SET_CURRENT_USER':
-            return {
-                ...state,
-                currentUser: action.payload
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case UserActionType.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
